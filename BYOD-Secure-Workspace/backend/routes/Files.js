0deaaa5386bca998239b21b8fce5df8b0d55aae4
Nikhil Damahe <damahenm@rknec.edu>
2026-03-08 11:49:05 +0530

    Update Files.js

--- BYOD-Secure-Workspace/backend/routes/Files.js
+++ BYOD-Secure-Workspace/backend/routes/Files.js
@@ -4,15 +4,15 @@ const path = require('path');
 
 const router = express.Router();
 
-// Define upload directory
+
 const uploadDir = path.join(__dirname, '../data/uploads');
 
-// Ensure upload folder exists
+
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
 
-// GET all uploaded files
+
 router.get('/', (req, res) => {
   fs.readdir(uploadDir, (err, files) => {
     if (err) return res.status(500).json({ message: 'Failed to read uploads' });
